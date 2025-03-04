3be7017de83a5291806732edaab32dcd3f888bab
mirsaddarici <mirsatdarici40@outlook.com>
2025-03-04 13:59:10 +0300

    düzenleme

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 def topla(s1,s2):
     return s1+s2
+
 def cikar(s1,s2):
     return s1-s2
+
 def carp(s1,s2):
     return s1*s2
+
 def bol(s1,s2):
     return s1/s2
+
 def bilgilendir():
     print("Toplama: 1, Çıkarma 2, Çarpma: 3, Bölme: 4, Bilgilendirme: i, Çıkış: q")
+
 print("Hesap Makinesi v1.0")
 bilgilendir()
+
 while True:
     islem = input("İşlem ID'si giriniz: ")
+
     if islem == "q":
         print("Program sonlandırılıyor... ")
         break
+
     elif str(islem) == "i":
         bilgilendir()
         continue
+
     sayi1 = int(input("İlk sayıyı giriniz: "))
     sayi2 = int(input("İkinci sayıyı giriniz: "))
+
     if islem == "1":
         print("Sonuç:",topla(sayi1,sayi2))
     elif islem == "2":
